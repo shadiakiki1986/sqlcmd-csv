f88342df034767c98f40b6875d722053fe9a73d6
Shadi Akiki <shadiakiki1986@gmail.com>
2022-02-17 18:10:17 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='sqlcmd_csv',
-    version="0.0.1,
+    version="0.0.1",
     author="Shadi Akiki",
     url='https://github.com/shadiakiki1986/ms-sqlcmd-csv',
     description="convert microsoft's sqlcmd output to valid csv",    
