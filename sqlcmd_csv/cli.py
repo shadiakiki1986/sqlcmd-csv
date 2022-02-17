dc470c872f0840f595fc308181862420e8b6fbfc
Shadi Akiki <shadiakiki1986@gmail.com>
2022-02-17 18:04:47 -0500

    Create cli.py

---
+++ sqlcmd_csv/cli.py
@@ -0,0 +1,20 @@
+from .core import FixedWidthSplitter
+import click
+import pandas as pd
+
+@click.command()
+@click.argument('input', type=click.File('rb'))
+@click.argument('output', type=click.File('wb'))
+def cli(input):
+    """Convert output from microsoft's sqlcmd to valid csv.
+    Solve https://stackoverflow.com/questions/425379/how-to-export-data-as-csv-format-from-sql-server-using-sqlcmd
+    Usage:
+    sqlcmd ... -o out.txt
+    sqlcmd_csv out.txt out.csv
+    """
+    result = input.readlines()
+    sp = FixedWidthSplitter(result[1])
+    header = sp.split(result[0])
+    body = sp.split(result[2:-3])
+    df = pd.DataFrame(body, columns=header)
+    df.to_csv(output)
