b816fa305ed2622478c2b61d57c5b436634b8cca
Shadi Akiki <shadiakiki1986@gmail.com>
2022-02-17 17:58:39 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='sqlcmd_csv',
+    version="0.0.1,
+    author="Shadi Akiki",
+    url='https://github.com/shadiakiki1986/ms-sqlcmd-csv',
+    description="convert microsoft's sqlcmd output to valid csv",    
+    packages=find_packages(),
+    install_requires=[
+      "pandas",
+    ],
+)
